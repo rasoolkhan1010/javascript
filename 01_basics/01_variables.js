6749393d9404d7d7973372fea603b0cbae2030d2
rasoolkhan1010 <rasoolkhan990880@gmail.com>
2026-07-15 12:31:45 +0000

    _05 js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445553
+let accountEmail = "rasoolkhan990880@gmail.com "
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//  accountId = 2 // not allowed , as this variable was already declaed as const in line no 1
+accountEmail = "rk@gmail.com"
+accountPassword= "21212121"
+accountCity= "hyderabad"
+let accountState;
+
+ console.log(accountId);
+   
+ console.table([accountEmail, accountId,accountPassword,accountCity,accountState])
+
